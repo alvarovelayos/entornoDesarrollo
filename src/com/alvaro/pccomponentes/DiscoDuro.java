522d9d521c5ee755e43ef52942eb997b8a981030
alvarovelayos <alvarovelayos31@gmail.com>
2022-10-17 22:06:25 +0200

    subido DiscoDuro y Ventilador

---
+++ src/com/alvaro/pccomponentes/DiscoDuro.java
@@ -0,0 +1,36 @@
+package com.alvaro.pccomponentes;
+
+public class DiscoDuro {
+    private Integer id;
+    private String marca;
+    private String fabricante;
+
+    public Integer getId(){
+
+        return id;
+    }
+
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+
+    public String getMarca() {
+
+        return marca;
+    }
+    public void setMarca(String marca){
+
+        this.marca = marca;
+    }
+
+    public String getFabricante() {
+
+        return fabricante;
+    }
+    public void setFabricante(String fabricante)
+    {
+
+        this.fabricante = fabricante;
+    }
+}
