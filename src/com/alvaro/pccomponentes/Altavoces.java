c80e2555031ee0226f4bc3d2f85e00daf8a3d6e8
alvarovelayos <alvarovelayos31@gmail.com>
2022-10-18 21:15:19 +0200

    subido Altavoces

---
+++ src/com/alvaro/pccomponentes/Altavoces.java
@@ -0,0 +1,37 @@
+package com.alvaro.pccomponentes;
+
+public class Altavoces {
+
+    private Integer id;
+    private String marca;
+    private String fabricante;
+
+    public Integer getId() {
+
+        return id;
+    }
+
+    public void setId(Integer id) {
+
+        this.id = id;
+    }
+
+    public String getMarca() {
+
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+
+        this.marca = marca;
+    }
+    public String getFabricante() {
+
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+
+        this.fabricante = fabricante;
+    }
+}
