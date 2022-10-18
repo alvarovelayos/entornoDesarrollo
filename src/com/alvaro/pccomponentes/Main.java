46ea471f21bdf3e6a98e4e0e74c8a79faf13ba03
alvarovelayos <alvarovelayos31@gmail.com>
2022-10-18 22:50:51 +0200

    subido Main

--- src/com/alvaro/pccomponentes/Main.java
+++ src/com/alvaro/pccomponentes/Main.java
@@ -131,6 +131,5 @@ public class Main {
         System.out.println(speeker.getId());
         System.out.println(speeker.getMarca());
         System.out.println(speeker.getFabricante());
-        System.out.println("hol");
     }
 }
